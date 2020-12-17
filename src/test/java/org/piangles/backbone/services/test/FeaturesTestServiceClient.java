0a16ff6cf576494b51ea253de3a8fead1e8b8c3e
saradhivs <saradhivs@yahoo.com>
2020-12-17 08:55:50 -0500

    Uses core test classes now.

--- src/test/java/org/piangles/backbone/services/test/FeaturesTestServiceClient.java
+++ src/test/java/org/piangles/backbone/services/test/FeaturesTestServiceClient.java
@@ -1,43 +1,34 @@
 package org.piangles.backbone.services.test;
 
 import org.piangles.backbone.services.Locator;
-import org.piangles.core.services.remoting.SessionAwareable;
-import org.piangles.core.services.remoting.SessionDetails;
 import org.piangles.core.stream.Stream;
+import org.piangles.core.test.AbstractServiceTestClient;
 
-public class FeaturesTestServiceClient extends Thread implements SessionAwareable
+public class FeaturesTestServiceClient extends AbstractServiceTestClient
 {
-	private static String cipherAuthorizationId = "7a948dce-1ebb-4770-b077-f453e60243da";
-
 	public static void main(String[] args) throws Exception
 	{
 		FeaturesTestServiceClient test = new FeaturesTestServiceClient();
 		test.start();
 	}
 
-	public void run()
+	public void runImpl() throws Exception
 	{
-		try
-		{
-			FeaturesTestService fts = Locator.getInstance().getFeaturesTestService();
-			Stream<String> stream = fts.getStringSequences();
-			
-			System.out.println("Metadata :: " + stream.getMetadata());
-			
-			stream.processAsync((payload) -> {
-				System.out.println("New message on stream : " + payload);
-				return payload;
-			});
-		}
-		catch (Exception e)
-		{
-			e.printStackTrace();
-		}
-	}
-
-	@Override
-	public SessionDetails getSessionDetails()
-	{
-		return new SessionDetails("FeaturesTestService", "TODOSessionId");
+		FeaturesTestService fts = Locator.getInstance().getFeaturesTestService();
+		Stream<String> stream = fts.getStringSequences();
+		
+		System.out.println("Metadata :: " + stream.getMetadata());
+		
+		stream.processAsync((payload) -> {
+			if (payload.isPresent())
+			{
+				System.out.println("New message on stream : " + payload.get());
+			}
+			else//End of Stream
+			{
+				System.out.println("This is the end of the stream");
+			}
+			return payload;
+		});
 	}
 }
